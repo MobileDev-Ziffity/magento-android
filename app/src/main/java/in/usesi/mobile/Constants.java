6c4f6d13db7d24570bf33140c868a70e7b5f12bf
Arun Kumar <arunkumar.p@ziffity.com>
2019-05-06 19:45:25 +0530

    Employee Login

---
+++ app/src/main/java/in/usesi/mobile/Constants.java
@@ -0,0 +1,12 @@
+package in.usesi.mobile;
+
+class Constants {
+    static String BASE_URL = "https://www.usesi.com/";
+    static String LOCATION_URL = "https://careers.usesi.com/companies";
+    static String CATEGORY_URL = "http://manage.hawksearch.com/sites/uselectric/?hawkoutput=json&hawkpath=category";
+    static Boolean apiCall = true;
+    static String VERSION_REGION = "usesi";
+    static String VERSION_CHECK  = "https://s3.amazonaws.com/magento-assets/mobile-app-versions.json";
+    static String SHOP_BY_LIST = "advancewishlist/index/rawlists/";
+    static String SERVICE_NUMBER = "(717)696-6426";
+}
