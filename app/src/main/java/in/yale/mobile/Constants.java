e3cea5a45579d799644cfa3df34004105d362544
Rajai Kumar <writetorajai@outlook.com>
2021-02-26 18:34:15 +0530

    Changed CS phone number for all  apps.

--- app/src/main/java/in/yale/mobile/Constants.java
+++ app/src/main/java/in/yale/mobile/Constants.java
@@ -6,5 +6,5 @@ class Constants {
     static String CATEGORY_URL = "https://manage.hawksearch.com/sites/uselectric/?hawkoutput=json&hawkpath=category";
     static Boolean apiCall = true;
     static String SHOP_BY_LIST = "advancewishlist/index/rawlists/";
-    static String SERVICE_NUMBER = "(717)696-6426";
+    static String SERVICE_NUMBER = BuildConfig.SERVICE_NUMBER;
 }
