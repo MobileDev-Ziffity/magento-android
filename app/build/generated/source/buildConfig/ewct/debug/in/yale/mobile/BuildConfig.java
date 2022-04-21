0e3d0d97a19090ee8b312d1e5a3f510ea9bec0be
hh <hh@gmail.com>
2022-04-21 13:35:35 +0530

    <1.41><Icons replacement>

---
+++ app/build/generated/source/buildConfig/ewct/debug/in/yale/mobile/BuildConfig.java
@@ -0,0 +1,25 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package in.yale.mobile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "in.ewct.mobile";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "ewct";
+  public static final int VERSION_CODE = 49;
+  public static final String VERSION_NAME = "1.44";
+  // Field from product flavor: ewct
+  public static final String BASE_URL = "https://www.ew-ct.com/";
+  // Field from product flavor: ewct
+  public static final String DOTDIGITAL_APISPACEID = "8999d9e6-d11b-4a5a-a6ba-0465fa33dc48";
+  // Field from product flavor: ewct
+  public static final String DOTDIGITAL_SECRET = "b6637e17fe58486bb6875309ae0b49be";
+  // Field from the variant API
+  public static final String FLAVOURED_AUTHORITIES = "in.ewct.mobile.fileprovider";
+  // Field from product flavor: ewct
+  public static final String LOCATION_URL = "https://www.ew-ct.com/locations/";
+  // Field from product flavor: ewct
+  public static final String SERVICE_NUMBER = "(860) 357-6064";
+}
